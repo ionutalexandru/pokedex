048c8b60fea6021e588728ef9838173e7f5ead1a
IonutAlexandru <cosuleanu@fazua.com>
2018-07-16 13:55:30 +0200

    Single & continuous fetching w componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -8,54 +8,77 @@ import {loadPokemons, createPokemon, fetchSinglePokemon} from './lib/pokeService
 import {addPokemon} from './lib/pokeHelpers'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      pokemons: [],
-      fetching: false,
-      pokeIdToFetch: 0,
-      basePokeUrl: "https://pokeapi.co/api/v2/pokemon/"
-    }
+  state = {
+    pokemons: [],
+    pokeIdToFetch: 0,
+    single_pokemon_fetching: false,
+    varios_pokemons_fetching: false,
   }
 
-  handleStartButton = () => {
-    this.setState({
-      fetching: true,
-    })
-    fetchSinglePokemon(this.state.pokeIdToFetch)
-      .then(response =>  {
-        const pokemon = {id: response.data.id, name: response.data.name, img: response.data.sprites.front_default}
-        const updatedPokemons = addPokemon(this.state.pokemons, pokemon)
-        this.setState({
-          pokemons: updatedPokemons,
-          pokeIdToFetch: this.state.pokeIdToFetch + 1,
-          fetching: false,
-        })
-        createPokemon(pokemon)
-      })
+  handleFetchSinglePokeButton = () => {
+    this.setState(
+      state => ({single_pokemon_fetching: !state.single_pokemon_fetching})
+    )
   }
 
   handleStopButton = () => {
     this.setState({
-      fetching: false,
+      single_pokemon_fetching: false,
+      varios_pokemons_fetching: false,
     })
+  }
+
+  handleFetchPokemonsButton = () => {
+    this.setState(
+      state => ({varios_pokemons_fetching: !state.varios_pokemons_fetching})
+    )
   }
 
   componentDidMount() {
     loadPokemons()
-      .then(pokemons => this.setState({
-        pokemons,
-        pokeIdToFetch: pokemons[pokemons.length - 1].id + 1,
-      })
+      .then(pokemons => {
+        this.setState({
+          pokemons,
+          pokeIdToFetch: pokemons.length ? pokemons[pokemons.length - 1].id + 1 : 1,
+        })
+      }
     )
+  }
+
+  componentDidUpdate = () => {
+    if(this.state.single_pokemon_fetching) {
+      fetchSinglePokemon(this.state.pokeIdToFetch)
+        .then(response =>  {
+          const pokemon = {id: response.data.id, name: response.data.name, img: response.data.sprites.front_default}
+          const updatedPokemons = addPokemon(this.state.pokemons, pokemon)
+          createPokemon(pokemon)
+          this.setState({
+            pokemons: updatedPokemons,
+            pokeIdToFetch: this.state.pokeIdToFetch + 1,
+            single_pokemon_fetching: false,
+          })
+        })
+    }else if (this.state.varios_pokemons_fetching) {
+      fetchSinglePokemon(this.state.pokeIdToFetch)
+        .then(response =>  {
+          const pokemon = {id: response.data.id, name: response.data.name, img: response.data.sprites.front_default}
+          createPokemon(pokemon)
+          const updatedPokemons = addPokemon(this.state.pokemons, pokemon)
+          this.setState({
+            pokemons: updatedPokemons,
+            pokeIdToFetch: this.state.pokeIdToFetch + 1,
+          })
+        })
+    }
   }
 
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
-        {this.state.fetching ? <button disabled onClick={this.handleStartButton}>Fetch a single Pokémon</button> : <button onClick={this.handleStartButton}>Fetch a single Pokémon</button>}
-        {this.state.fetching ? <button onClick={this.handleStopButton}>Stop Fetching</button> : <button disabled onClick={this.handleStopButton}>Stop Fetching</button>}
+        {this.state.single_pokemon_fetching | this.state.varios_pokemons_fetching ? <button disabled onClick={this.handleFetchSinglePokeButton}>Fetch a single Pokémon</button> : <button onClick={this.handleFetchSinglePokeButton}>Fetch a single Pokémon</button>}
+        {this.state.single_pokemon_fetching | this.state.varios_pokemons_fetching ? <button disabled onClick={this.handleFetchPokemonsButton}>Fetch Pokémons</button> : <button onClick={this.handleFetchPokemonsButton}>Fetch Pokémons</button>}
+        {this.state.single_pokemon_fetching | this.state.varios_pokemons_fetching ? <button onClick={this.handleStopButton}>Stop Fetching</button> : <button disabled onClick={this.handleStopButton}>Stop Fetching</button>}
         <ul>
           {this.state.pokemons.map(pokemon => <PokeCard key={pokemon.id} name={pokemon.name} img={pokemon.img}/>)}
         </ul>
