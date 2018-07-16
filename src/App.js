3d81c1d96888f6df09d93c5d476bc1a4ba6cdc28
IonutAlexandru <cosuleanu@fazua.com>
2018-07-16 10:54:43 +0200

    Fetch a single Pokemon in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from "react";
+import React, {Component} from "react"
+import axios from 'axios'
 
 import './App.css';
 
@@ -14,6 +15,8 @@ class App extends Component {
         {id: 3, name: "venusaur" , img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/3.png"}
       ],
       fetching: false,
+      pokeIdToFetch: 4,
+      basePokeUrl: "https://pokeapi.co/api/v2/pokemon/"
     }
   }
 
@@ -21,6 +24,16 @@ class App extends Component {
     this.setState({
       fetching: true,
     })
+    axios.get(this.state.basePokeUrl + this.state.pokeIdToFetch)
+      .then(response => {
+        const {pokemons} = this.state.pokemons
+        let pokemon = {id: response.data.id, name: response.data.name, img: response.data.sprites.front_default}
+        this.setState({
+          pokemons: [...this.state.pokemons, pokemon],
+          pokeIdToFetch: this.state.pokeIdToFetch + 1,
+          fetching: false,
+        })
+      })
   }
 
   handleStopButton = () => {
@@ -33,7 +46,7 @@ class App extends Component {
     return (
       <div>
         <h1>Hello World!</h1>
-        {this.state.fetching ? <button disabled onClick={this.handleStartButton}>Start Fetching</button> : <button onClick={this.handleStartButton}>Start Fetching</button>}
+        {this.state.fetching ? <button disabled onClick={this.handleStartButton}>Fetch a single Pokémon</button> : <button onClick={this.handleStartButton}>Fetch a single Pokémon</button>}
         {this.state.fetching ? <button onClick={this.handleStopButton}>Stop Fetching</button> : <button disabled onClick={this.handleStopButton}>Stop Fetching</button>}
         <ul>
           {this.state.pokemons.map(pokemon => <PokeCard key={pokemon.id} name={pokemon.name} img={pokemon.img}/>)}
