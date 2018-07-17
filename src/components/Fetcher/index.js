72a11ed65589abfe4904e79d839a399111ff61df
IonutAlexandru <cosuleanu@fazua.com>
2018-07-17 22:54:54 +0200

    Added more style

--- src/components/Fetcher/index.js
+++ src/components/Fetcher/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 import RaisedButton from 'material-ui/RaisedButton'
+import FileCloudDownload from 'material-ui/svg-icons/file/cloud-download'
+import Stop from 'material-ui/svg-icons/av/stop'
+import Clear from 'material-ui/svg-icons/content/clear'
 
 const buttonStyle = {
   marginRight: '10px',
@@ -13,21 +16,25 @@ export const Fetcher = (props) => {
       <RaisedButton
         label = "Fetch a Single Pokémon"
         style = {buttonStyle}
+        icon = {<FileCloudDownload/>}
         onClick = {props.handleFetchSinglePokemon}
       />
       <RaisedButton
         label = "Fetch All Pokémons"
         style = {buttonStyle}
+        icon = {<FileCloudDownload/>}
         onClick = {props.handleFetchAllPokemons}
       />
       <RaisedButton
         label = "Stop Fetching"
         style = {buttonStyle}
+        icon = {<Stop/>}
         onClick = {props.handleStopFetching}
       />
       <RaisedButton
         label = "Clear Storage"
         style = {buttonStyle}
+        icon = {<Clear/>}
         onClick = {props.handleClearStorage}
       />
     </div>
