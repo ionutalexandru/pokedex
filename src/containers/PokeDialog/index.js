5ac3290a6107e5102f84568c6c88e10ad7eea611
IonutAlexandru <cosuleanu@fazua.com>
2018-07-19 19:10:20 +0200

    PokeDialog container default

---
+++ src/containers/PokeDialog/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+import {PokeDialog} from '/components/PokeDialog'
+
+export default class PokeDialog extends Component {
+  render() {
+    return(
+      <PokeDialog />
+    )
+  }
+}
