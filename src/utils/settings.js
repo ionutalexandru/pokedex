578f022b0765586d7955ce2f78180e7f0d1d065a
IonutAlexandru <cosuleanu@fazua.com>
2018-08-12 12:38:43 +0200

    Define settings where needed settings of the web app is written down

---
+++ src/utils/settings.js
@@ -0,0 +1,54 @@
+export const getTypeColor = {
+    normal: '#A8A878',
+    fire: '#F08030',
+    fighting: '#C03028',
+    water: '#6890F0',
+    grass: '#78C850',
+    poison: '#A040A0',
+    electric: '#F8D030',
+    ground: '#E0C068',
+    rock: '#B8A038',
+    bug: '#A8B820',
+    dragon: '#7038F8',
+    ghost: '#705898',
+    dark: '#705848',
+    fairy: '#EE99AC',
+    steel: '#B8B8D0',
+    psychic: '#F85888',
+    ice: '#98D8D8',
+    flying: '#A890F0',
+}
+
+export const endpoints = {
+  pokemon: {
+    pokemon: "https://pokeapi.co/api/v2/pokemon/",
+    species: "https://pokeapi.co/api/v2/pokemon-species/",
+  },
+  db: {
+    endpoint: "http://localhost:3000",
+    path: "pokemons",
+  }
+}
+
+export const pokemonTypes = [
+  'normal',
+  'fighting',
+  'flying',
+  'poison',
+  'ground',
+  'rock',
+  'bug',
+  'ghost',
+  'steel',
+  'fire',
+  'water',
+  'grass',
+  'electric',
+  'psychic',
+  'ice',
+  'dragon',
+  'dark',
+  'fairy',
+  'unknown',
+  'shadow'
+]
